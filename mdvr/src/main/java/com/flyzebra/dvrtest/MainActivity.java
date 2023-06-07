18b66f0dd014a6e50c93702ea9c48470ccf4a44d
flyzebra <flyzebra@outlook.com>
2023-06-07 11:28:58 +0800

    ioctl test

--- mdvr/src/main/java/com/flyzebra/dvrtest/MainActivity.java
+++ mdvr/src/main/java/com/flyzebra/dvrtest/MainActivity.java
@@ -4,18 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.SurfaceView;
-import android.widget.TextView;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.flyzebra.dvrtest.databinding.ActivityMainBinding;
+import com.flyzebra.utils.FlyLog;
 import com.quectel.qcarapi.stream.QCarCamera;
 
 public class MainActivity extends AppCompatActivity {
 
     private SurfaceView sv01;
+    private QCarCamera qCarCamera;
 
     // Used to load the 'dvrtest' library on application startup.
     static {
-        System.loadLibrary("dvrtest");
+        System.loadLibrary("mdvr_zebra");
+        System.loadLibrary("mmqcar_qcar_jni");
     }
 
     private ActivityMainBinding binding;
@@ -23,28 +27,41 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         sv01 = findViewById(R.id.sv01);
 
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+        cameraPowerUp();
 
-        try {
-            QCarCamera qCarCamera = new QCarCamera(1);
-            int ret = qCarCamera.cameraOpen(1, 2);
-            qCarCamera.startPreview(0, sv01.getHolder().getSurface(), 720, 1280, QCarCamera.YUV420_NV21);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        //qCarCamera = new QCarCamera(1);
+        //int ret = qCarCamera.cameraOpen(1, 2);
+        //if (ret == 0) {
+        //    qCarCamera.startPreview(0, sv01.getHolder().getSurface(), 720, 1280, QCarCamera.YUV420_NV21);
+        //} else if (ret > 0) {
+        //    FlyLog.e("camera don't close, ret=%d", ret);
+        //} else {
+        //    FlyLog.e("camera open failed, ret=%d", ret);
+        //}
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //qCarCamera.stopPreview(0);
+        //qCarCamera.cameraClose();
+
+        cameraPowerDown();
     }
 
     /**
      * A native method that is implemented by the 'dvrtest' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public native void cameraPowerUp();
+
+    public native void cameraPowerDown();
 }
