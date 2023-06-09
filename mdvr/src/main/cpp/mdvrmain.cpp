92662e276b49457632b7dcd4b56270b6e36281bf
flyzebra <flyzebra@outlook.com>
2023-06-09 17:29:33 +0800

    test android9 ok

--- mdvr/src/main/cpp/mdvrmain.cpp
+++ mdvr/src/main/cpp/mdvrmain.cpp
@@ -11,14 +11,45 @@
 
 #define BASE_VIDIOC_PRIVATE	192		/* 192-255 are private */
 #define MAX_SENSOR_NAME 32
-#define CFG_POWER_UP                  47
-#define CFG_POWER_DOWN                48
 
 enum camb_position_t {
     BACK_CAMERA_B,
     FRONT_CAMERA_B,
     AUX_CAMERA_B = 0x100,
     INVALID_CAMERA_B,
+};
+
+enum msm_sensor_cfg_type_t {
+    CFG_SET_SLAVE_INFO,
+    CFG_SLAVE_READ_I2C,
+    CFG_WRITE_I2C_ARRAY,
+    CFG_SLAVE_WRITE_I2C_ARRAY,
+    CFG_WRITE_I2C_SEQ_ARRAY,
+    CFG_POWER_UP,
+    CFG_POWER_DOWN,
+    CFG_SET_STOP_STREAM_SETTING,
+    CFG_GET_SENSOR_INFO,
+    CFG_GET_SENSOR_INIT_PARAMS,
+    CFG_SET_INIT_SETTING,
+    CFG_SET_RESOLUTION,
+    CFG_SET_STOP_STREAM,
+    CFG_SET_START_STREAM,
+    CFG_SET_SATURATION,
+    CFG_SET_CONTRAST,
+    CFG_SET_SHARPNESS,
+    CFG_SET_ISO,
+    CFG_SET_EXPOSURE_COMPENSATION,
+    CFG_SET_ANTIBANDING,
+    CFG_SET_BESTSHOT_MODE,
+    CFG_SET_EFFECT,
+    CFG_SET_WHITE_BALANCE,
+    CFG_SET_AUTOFOCUS,
+    CFG_CANCEL_AUTOFOCUS,
+    CFG_SET_STREAM_TYPE,
+    CFG_SET_I2C_SYNC_PARAM,
+    CFG_WRITE_I2C_ARRAY_ASYNC,
+    CFG_WRITE_I2C_ARRAY_SYNC,
+    CFG_WRITE_I2C_ARRAY_SYNC_BLOCK,
 };
 
 enum sensor_sub_module_t {
@@ -56,38 +87,80 @@ struct sensorb_cfg_data {
     struct msm_sensor_info_t  sensor_info;
 };
 
+enum csid_cfg_type_t {
+    CSID_INIT,
+    CSID_CFG,
+    CSID_TESTMODE_CFG,
+    CSID_RELEASE,
+};
+
+struct csid_cfg_data {
+    enum csid_cfg_type_t cfgtype;
+    uint32_t csid_version;
+};
+
 #define VIDIOC_MSM_SENSOR_CFG \
 	_IOWR('V', BASE_VIDIOC_PRIVATE + 1, struct sensorb_cfg_data)
+
+#define VIDIOC_MSM_CSID_IO_CFG \
+	_IOWR('V', BASE_VIDIOC_PRIVATE + 5, struct csid_cfg_data)
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_flyzebra_dvrtest_MainActivity_cameraPowerUp(JNIEnv *env, jobject thiz) {
-    struct sensorb_cfg_data cfg;
-    cfg.cfgtype = CFG_POWER_UP;
-    int fd = open("/dev/rn6864m_csi1", O_RDWR | O_NDELAY | O_NOCTTY);
-    if (fd > 0) {
-        int ret = ioctl(fd, VIDIOC_MSM_SENSOR_CFG, &cfg);
+    int fd1 = open("/dev/v4l-subdev17", O_RDWR);
+    if (fd1 > 0) {
+        struct sensorb_cfg_data cfg;
+        cfg.cfgtype = CFG_POWER_UP;
+        int ret = ioctl(fd1, VIDIOC_MSM_SENSOR_CFG, &cfg);
         if(ret < 0){
-            FLOGE("ioctl power up failed! fd=%d, ret=%d", fd, ret);
+            FLOGE("ioctl power up failed! fd=%d, ret=%d", fd1, ret);
         }
-        close(fd);
+        close(fd1);
     }else{
-        FLOGE("open /dev/rn6864m_csi1 failed!");
+        FLOGE("open /dev/v4l-subdev17 failed!");
+    }
+    //int fd2 = open("/dev/v4l-subdev5", O_RDWR);
+    //if (fd2 > 0) {
+    //    struct csid_cfg_data cfg;
+    //    cfg.cfgtype = CSID_INIT;
+    //    int ret = ioctl(fd2, VIDIOC_MSM_CSID_IO_CFG, &cfg);
+    //    if(ret < 0){
+    //        FLOGE("ioctl csid init failed! fd=%d, ret=%d", fd2, ret);
+    //    }
+    //    close(fd2);
+    //}else{
+    //    FLOGE("open /dev/v4l-subdev5 failed!");
+    //}
+    int fd3 = open("/dev/v4l-subdev17", O_RDWR);
+    if (fd3 > 0) {
+        for (int i = 0; i < 3; i++) {
+            struct sensorb_cfg_data cfg1;
+            cfg1.cfgtype = CFG_WRITE_I2C_ARRAY;
+            int ret = ioctl(fd3, VIDIOC_MSM_SENSOR_CFG, &cfg1);
+            if (ret < 0) {
+                FLOGE("ioctl power up failed! fd=%d, ret=%d", fd3, ret);
+            }
+        }
+        close(fd3);
+    }else{
+        FLOGE("open /dev/v4l-subdev17 failed!");
     }
 }
+
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_flyzebra_dvrtest_MainActivity_cameraPowerDown(JNIEnv *env, jobject thiz) {
-    struct sensorb_cfg_data cfg;
-    cfg.cfgtype = CFG_POWER_DOWN;
-    int fd = open("/dev/rn6864m_csi1", O_RDWR | O_NDELAY | O_NOCTTY);
+    int fd = open("/dev/v4l-subdev17", O_RDWR | O_NDELAY | O_NOCTTY);
     if (fd > 0) {
+        struct sensorb_cfg_data cfg;
+        cfg.cfgtype = CFG_POWER_DOWN;
         int ret = ioctl(fd, VIDIOC_MSM_SENSOR_CFG, &cfg);
         if(ret < 0){
-            FLOGE("ioctl power down failed! ret=%d", ret);
+            FLOGE("ioctl power down failed! fd=%d, ret=%d", fd, ret);
         }
         close(fd);
     }else{
-        FLOGE("open /dev/rn6864m_csi1 failed!");
+        FLOGE("open /dev/v4l-subdev17 failed!");
     }
 }
--- mdvr/src/main/java/com/flyzebra/dvrtest/MainActivity.java
+++ mdvr/src/main/java/com/flyzebra/dvrtest/MainActivity.java
@@ -1,8 +1,10 @@
 package com.flyzebra.dvrtest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.Window;
 import android.view.WindowManager;
@@ -12,9 +14,11 @@ import com.flyzebra.utils.FlyLog;
 import com.quectel.qcarapi.stream.QCarCamera;
 
 public class MainActivity extends AppCompatActivity {
-
-    private SurfaceView sv01;
-    private QCarCamera qCarCamera;
+    private SurfaceView sv01 = null;
+    private SurfaceView sv02 = null;
+    private SurfaceView sv03 = null;
+    private SurfaceView sv04 = null;
+    private QCarCamera qCarCamera = null;
 
     // Used to load the 'dvrtest' library on application startup.
     static {
@@ -33,28 +37,122 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         sv01 = findViewById(R.id.sv01);
+        sv02 = findViewById(R.id.sv02);
+        sv03 = findViewById(R.id.sv03);
+        sv04 = findViewById(R.id.sv04);
+        //cameraPowerUp();
+        qCarCamera = new QCarCamera(1);
+        int ret = qCarCamera.cameraOpen(4, 3);
+        if (ret == 0) {
+            FlyLog.e("camera open success!");
+        } else if (ret > 0) {
+            FlyLog.e("camera don't close, ret=%d", ret);
+        } else {
+            FlyLog.e("camera open failed, ret=%d", ret);
+        }
 
-        cameraPowerUp();
+        sv01.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder holder) {
+                if (qCarCamera != null) {
+                    qCarCamera.startPreview(0, holder.getSurface(), 1280, 720, QCarCamera.YUV420_NV21);
+                }
+            }
 
-        //qCarCamera = new QCarCamera(1);
-        //int ret = qCarCamera.cameraOpen(1, 2);
-        //if (ret == 0) {
-        //    qCarCamera.startPreview(0, sv01.getHolder().getSurface(), 720, 1280, QCarCamera.YUV420_NV21);
-        //} else if (ret > 0) {
-        //    FlyLog.e("camera don't close, ret=%d", ret);
-        //} else {
-        //    FlyLog.e("camera open failed, ret=%d", ret);
-        //}
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+                if (qCarCamera != null) {
+                    qCarCamera.stopPreview(0);
+                }
+            }
+        });
+
+
+        sv02.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder holder) {
+                if (qCarCamera != null) {
+                    qCarCamera.startPreview(1, holder.getSurface(), 1280, 720, QCarCamera.YUV420_NV21);
+                }
+            }
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+                if (qCarCamera != null) {
+                    qCarCamera.stopPreview(1);
+                }
+            }
+        });
+
+        sv03.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder holder) {
+                if (qCarCamera != null) {
+                    qCarCamera.startPreview(2, holder.getSurface(), 1280, 720, QCarCamera.YUV420_NV21);
+                }
+            }
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+                if (qCarCamera != null) {
+                    qCarCamera.stopPreview(2);
+                }
+            }
+        });
+
+        sv04.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder holder) {
+                if (qCarCamera != null) {
+                    qCarCamera.startPreview(3, holder.getSurface(), 1280, 720, QCarCamera.YUV420_NV21);
+                }
+            }
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+                if (qCarCamera != null) {
+                    qCarCamera.stopPreview(3);
+                }
+            }
+        });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
 
     @Override
     protected void onDestroy() {
+        //cameraPowerDown();
+        qCarCamera.cameraClose();
+        qCarCamera.release();
         super.onDestroy();
-        //qCarCamera.stopPreview(0);
-        //qCarCamera.cameraClose();
-
-        cameraPowerDown();
     }
 
     /**
@@ -64,4 +162,5 @@ public class MainActivity extends AppCompatActivity {
     public native void cameraPowerUp();
 
     public native void cameraPowerDown();
+
 }
